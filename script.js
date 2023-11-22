309efced44708417f38e4d8bea350424d7ee23cd
aishvarya252 <134908139+aishvarya252@users.noreply.github.com>
2023-11-22 08:37:24 +0530

    Updated script.js to connect to server

--- script.js
+++ script.js
@@ -1,89 +1,99 @@
-const canvas = document.getElementById("paintCanvas");
-const ctx = canvas.getContext("2d");
-const colorPicker = document.getElementById("colorPicker");
-const penButton = document.getElementById("penButton");
-const eraserButton = document.getElementById("eraserButton");
-const rectangleButton = document.getElementById("rectangleButton");
-const circleButton = document.getElementById("circleButton");
+// You can call the draw function with the coordinates received from the server
+var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-let isDrawing = false;
-let isErasing = false;
-let isDrawingRectangle = false;
-let isDrawingCircle = false;
-let lastX = 0;
-let lastY = 0;
-let shapeStartX = 0;
-let shapeStartY = 0;
+        // Handle incoming video frames
+        socket.on('video_frame', function(data) {
+            var img = document.getElementById('video_feed');
+            img.src = 'data:image/jpeg;base64,' + data.image;
+            console.log(data.coordinates, data.draw);
+        });
 
-canvas.width = window.innerWidth - 20;
-canvas.height = window.innerHeight - 150;
+        const canvas = document.createElement("canvas");
+        document.body.appendChild(canvas);
+        const ctx = canvas.getContext("2d");
+        const colorPicker = document.getElementById("colorPicker");
+        const penButton = document.getElementById("penButton");
+        const eraserButton = document.getElementById("eraserButton");
+        const rectangleButton = document.getElementById("rectangleButton");
+        const circleButton = document.getElementById("circleButton");
 
-canvas.addEventListener("mousedown", (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-    if (isErasing) ctx.clearRect(lastX - 10, lastY - 10, 20, 20);
-});
+        let isDrawing = false;
+        let isErasing = false;
+        let isDrawingRectangle = false;
+        let isDrawingCircle = false;
+        let lastX = 0;
+        let lastY = 0;
+        let shapeStartX = 0;
+        let shapeStartY = 0;
 
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mouseup", () => isDrawing = false);
-canvas.addEventListener("mouseout", () => isDrawing = false);
+        canvas.width = window.innerWidth - 20;
+        canvas.height = window.innerHeight - 150;
 
-penButton.addEventListener("click", () => {
-    isErasing = false;
-    isDrawingRectangle = false;
-    isDrawingCircle = false;
-    canvas.style.cursor = "crosshair";
-});
+        canvas.addEventListener("mousedown", (e) => {
+            isDrawing = true;
+            [lastX, lastY] = [e.offsetX, e.offsetY];
+            if (isErasing) ctx.clearRect(lastX - 10, lastY - 10, 20, 20);
+        });
 
-eraserButton.addEventListener("click", () => {
-    isErasing = true;
-    isDrawingRectangle = false;
-    isDrawingCircle = false;
-    canvas.style.cursor = "url('eraser.png'), auto";
-});
+        canvas.addEventListener("mousemove", draw);
+        canvas.addEventListener("mouseup", () => isDrawing = false);
+        canvas.addEventListener("mouseout", () => isDrawing = false);
 
-rectangleButton.addEventListener("click", () => {
-    isErasing = false;
-    isDrawingRectangle = true;
-    isDrawingCircle = false;
-    canvas.style.cursor = "crosshair";
-});
+        penButton.addEventListener("click", () => {
+            isErasing = false;
+            isDrawingRectangle = false;
+            isDrawingCircle = false;
+            canvas.style.cursor = "crosshair";
+        });
 
-circleButton.addEventListener("click", () => {
-    isErasing = false;
-    isDrawingRectangle = false;
-    isDrawingCircle = true;
-    canvas.style.cursor = "crosshair";
-});
+        eraserButton.addEventListener("click", () => {
+            isErasing = true;
+            isDrawingRectangle = false;
+            isDrawingCircle = false;
+            canvas.style.cursor = "url('eraser.png'), auto";
+        });
 
-colorPicker.addEventListener("input", () => {
-    ctx.strokeStyle = colorPicker.value;
-    ctx.fillStyle = colorPicker.value;
-});
+        rectangleButton.addEventListener("click", () => {
+            isErasing = false;
+            isDrawingRectangle = true;
+            isDrawingCircle = false;
+            canvas.style.cursor = "crosshair";
+        });
 
-function draw(e) {
-    if (!isDrawing) return;
+        circleButton.addEventListener("click", () => {
+            isErasing = false;
+            isDrawingRectangle = false;
+            isDrawingCircle = true;
+            canvas.style.cursor = "crosshair";
+        });
 
-    ctx.lineWidth = 2;
-    ctx.lineCap = "round";
+        colorPicker.addEventListener("input", () => {
+            ctx.strokeStyle = colorPicker.value;
+            ctx.fillStyle = colorPicker.value;
+        });
 
-    if (isErasing) {
-        ctx.clearRect(e.offsetX - 10, e.offsetY - 10, 20, 20);
-    } else if (isDrawingRectangle) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.strokeRect(shapeStartX, shapeStartY, e.offsetX - shapeStartX, e.offsetY - shapeStartY);
-    } else if (isDrawingCircle) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        const radius = Math.sqrt(Math.pow(e.offsetX - shapeStartX, 2) + Math.pow(e.offsetY - shapeStartY, 2));
-        ctx.beginPath();
-        ctx.arc(shapeStartX, shapeStartY, radius, 0, 2 * Math.PI);
-        ctx.stroke();
-    } else {
-        ctx.beginPath();
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
-        [lastX, lastY] = [e.offsetX, e.offsetY];
-    }
-}
+        function draw(e) {
+            if (!isDrawing) return;
 
+            ctx.lineWidth = 2;
+            ctx.lineCap = "round";
+
+            if (isErasing) {
+                ctx.clearRect(e.offsetX - 10, e.offsetY - 10, 20, 20);
+            } else if (isDrawingRectangle) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.strokeRect(shapeStartX, shapeStartY, e.offsetX - shapeStartX, e.offsetY - shapeStartY);
+            } else if (isDrawingCircle) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                const radius = Math.sqrt(Math.pow(e.offsetX - shapeStartX, 2) + Math.pow(e.offsetY - shapeStartY, 2));
+                ctx.beginPath();
+                ctx.arc(shapeStartX, shapeStartY, radius, 0, 2 * Math.PI);
+                ctx.stroke();
+            } else {
+                ctx.beginPath();
+                ctx.moveTo(lastX, lastY);
+                ctx.lineTo(e.offsetX, e.offsetY);
+                ctx.stroke();
+                [lastX, lastY] = [e.offsetX, e.offsetY];
+            }
+        }const canvas = document.getElementById("paintCanvas");
